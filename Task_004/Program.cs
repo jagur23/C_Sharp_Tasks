cf6255d0a7b2d2d610d7d14911d699349e88fa49
Dmitrijs Jagurs <dmitrijsjagurs@gmail.com>
2023-08-31 16:49:34 +0300

    Add Task 004

---
+++ Task_004/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Insert first number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Insert second number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Insert third number: ");
+double num3 = Convert.ToDouble(Console.ReadLine());
+
+if(num1 > num2 && num1 > num3)
+{
+    Console.Write("Max number is " + num1);
+}
+if(num2 > num1 && num2 > num3)
+{
+    Console.Write("Max number is " + num2);
+}
+if(num3 > num1 && num3 > num2)
+{
+    Console.Write("Max number is " + num3);
+}
