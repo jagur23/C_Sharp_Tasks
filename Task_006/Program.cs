e2ed2f1dd2795ed28c8619ff12a3ff666079058f
Dmitrijs Jagurs <dmitrijsjagurs@gmail.com>
2023-08-31 17:15:29 +0300

    Add Task 006

---
+++ Task_006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Insert number please: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("YES! An even number!");
+}
+else
+{
+    Console.WriteLine("NO! Not even number!");
+}
