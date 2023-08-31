beb426581d801d694394644ba96875744c810259
Dmitrijs Jagurs <dmitrijsjagurs@gmail.com>
2023-08-31 16:39:59 +0300

    Add Task 002

---
+++ Task_002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Insert first number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Insert second number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.Write("Max = " + num1);
+}
+else
+{
+    Console.Write("Max = " + num2);
+}
