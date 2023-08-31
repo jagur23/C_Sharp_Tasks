d0302bda5262c6f6e6f177461f15e4db0495f69b
Dmitrijs Jagurs <dmitrijsjagurs@gmail.com>
2023-08-31 18:23:53 +0300

    BugFix Task 002

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -3,9 +3,13 @@ double num1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Insert second number: ");
 double num2 = Convert.ToDouble(Console.ReadLine());
 
-if(num1 > num2)
+if (num1 > num2)
 {
     Console.Write("Max = " + num1);
+}
+else if (num1 == num2)
+{
+    Console.Write("Numbers are same");
 }
 else
 {
