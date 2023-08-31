0465422313d07bccc692c9654808ff6dd320180c
Dmitrijs Jagurs <dmitrijsjagurs@gmail.com>
2023-08-31 18:16:12 +0300

    Add Task 008

---
+++ Task_008/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Insert number bigger then 1: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+if(N < 2) 
+Console.Write("Incorrect number!");
+
+while (count <= N)
+{
+    Console.Write(count + ", ");
+    count = count + 2;
+}
