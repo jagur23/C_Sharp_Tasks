fe3103c1354ab651445d598c97b7a44789ed330b
Dmitrijs Jagurs <dmitrijsjagurs@gmail.com>
2023-09-04 15:04:51 +0300

    Add Task 010

---
+++ Task_010/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на 
+вход трёхзначное число и на выходе показывает вторую 
+цифру этого числа.
+*/
+
+Console.WriteLine("Enter a three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99 && num < 1000)
+{
+    Console.WriteLine(num % 100 / 10);
+}
+else
+{
+    Console.WriteLine("Not a three-digit number!");
+}
