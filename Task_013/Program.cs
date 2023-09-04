f04fe0dbcfb21b3caed54d8d8335fe341b453530
Dmitrijs Jagurs <dmitrijsjagurs@gmail.com>
2023-09-04 16:14:01 +0300

    Add Task 013

---
+++ Task_013/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+    Задача 13: Напишите программу, которая выводит 
+    третью цифру заданного числа или сообщает, что третьей 
+    цифры нет.
+*/
+
+Console.WriteLine("Enter a number please: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number > 999)
+{
+    number /= 10;
+    number = number % 10;
+    Console.WriteLine("Third digit is: " + number);
+}
+
+if (number > 99 && number < 1000)
+{
+    Console.WriteLine("Third digit is: " + number % 10);
+}
+else
+{
+    Console.WriteLine("Third digit does not exist!");
+}
+
