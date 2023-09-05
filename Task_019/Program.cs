d8eb1224bc9b789dd55fa9997ee130f1ae71a01f
Dmitrijs Jagurs <dmitrijsjagurs@gmail.com>
2023-09-05 19:09:31 +0300

    Add Task 019

---
+++ Task_019/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+    Задача 19: Напишите программу, которая принимает на 
+    вход пятизначное число и проверяет, является ли оно 
+    палиндромом.
+*/
+
+Console.Write("Enter 5-digit number >>> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if(number > 9999 && number < 100000)
+{
+    if(number / 10000 == number % 10 && number / 1000 % 10 == number % 100 / 10)
+    {
+        Console.WriteLine("This number is polyndrome!");
+    }
+    else
+    {
+        Console.WriteLine("This number is not a polyndrome!");
+    }
+}
+else
+{
+    Console.WriteLine("This is not a 5-digit number! Try again.");
+}
