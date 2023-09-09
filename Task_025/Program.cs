f6f16a16e919bd04e78625e89d682dd75fc2b5d5
Dmitrijs Jagurs <dmitrijsjagurs@gmail.com>
2023-09-10 01:45:33 +0300

    Add Task 025

---
+++ Task_025/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+    Задача 25: Напишите цикл, который принимает на вход 
+    два числа (A и B) и возводит число A в натуральную 
+    степень B.
+    3, 5 -> 243 (3⁵)
+    2, 4 -> 16
+*/
+
+Console.Clear();
+
+double Exponentiation(int A, int B)
+{
+    double result = Math.Pow(A, B);
+    Console.Write($"{A}, {B} -> {result}");
+    return result;
+}
+
+double num = Exponentiation(3, 5);
