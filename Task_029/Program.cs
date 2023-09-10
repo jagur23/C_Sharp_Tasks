af01370027408ddbc0877fd2f4d50be2f565a29c
Dmitrijs Jagurs <dmitrijsjagurs@gmail.com>
2023-09-10 15:37:54 +0300

    Add Task 029

---
+++ Task_029/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+    Задача 29: Напишите программу, которая задаёт массив 
+    из 8 элементов и выводит их на экран.
+
+    1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
+    6, 1, 33 -> [6, 1, 33]
+*/
+
+Console.Clear();
+
+int[] array = new int[8];
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+}
+
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}, ");
+}
+
+Console.Write("-> [");
+
+foreach(int element in array)
+{
+    Console.Write(element + ", ");
+}
+
+Console.Write("]");
