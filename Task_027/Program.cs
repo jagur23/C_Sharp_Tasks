6d2476182eba4d61eef414f517bd9859123e02b1
Dmitrijs Jagurs <dmitrijsjagurs@gmail.com>
2023-09-10 13:55:10 +0300

    Add Task 027

---
+++ Task_027/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+    Задача 27: Напишите программу, которая принимает на 
+    вход число и выдаёт сумму цифр в числе.
+    452 -> 11
+    82 -> 10
+    9012 -> 12
+*/
+
+Console.Clear();
+
+int DigitSum(string number)
+{
+    int result = 0;
+
+    for(int i = 0; i < number.Length; i++)
+    {
+        result = result + int.Parse(number[i].ToString());
+    }
+    return result;
+}
+
+Console.Write("Enter natural number: ");
+string number = Console.ReadLine()!;
+int num = int.Parse(number);
+
+if(num > 0)
+{
+    int sum = DigitSum(number);
+    Console.WriteLine($"{number} -> {sum}");
+}
+else
+{
+    Console.WriteLine("Wrong number. Try again :)");
+}
