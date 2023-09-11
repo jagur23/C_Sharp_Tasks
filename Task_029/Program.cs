2c0a8bb43341ad9b99af8060d799d33fbb654914
Dmitrijs Jagurs <dmitrijsjagurs@gmail.com>
2023-09-11 19:04:11 +0300

    BugFix Task 029

--- Task_029/Program.cs
+++ Task_029/Program.cs
@@ -8,6 +8,7 @@
 
 Console.Clear();
 
+/*
 int[] array = new int[8];
 
 for(int i = 0; i < array.Length; i++)
@@ -28,3 +29,47 @@ foreach(int element in array)
 }
 
 Console.Write("]");
+*/
+
+int Request(string message)
+{
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine()!);
+    return num;
+}
+
+int [] GenerateArrey(int length, int min, int max)
+{
+    int [] arrey = new int[length];
+
+    for(int i = 0; i < length; i++)
+    {
+        arrey[i] = new Random().Next(min, max + 1);
+    }
+    return arrey;
+}
+
+void PrintArray(int [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+
+    Console.Write("-> [");
+
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+
+    Console.Write($"{array[array.Length - 1]}");
+    Console.Write("]");
+}
+
+int arrLength = Request("Enter arrey length: ");
+int min = Request("Enter the value of the minimum random number: ");
+int max = Request("Enter the value of the maximum random number: ");
+
+int [] myArray = GenerateArrey(arrLength, min, max);
+PrintArray(myArray);
