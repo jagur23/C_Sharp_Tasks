127684dd832176c0565c536a58a705f02a0a8de0
Dmitrijs Jagurs <dmitrijsjagurs@gmail.com>
2023-09-17 16:29:17 +0300

    Add Task 041

---
+++ Task_041/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+    Задача 41: Пользователь вводит с клавиатуры M чисел. 
+    Посчитайте, сколько чисел больше 0 ввёл пользователь.
+    0, 7, 8, -2, -2 -> 2
+    -1, -7, 567, 89, 223-> 3
+*/
+
+Console.Clear();
+
+int PositiveNumCount(string numArray)
+{
+    string[] strArr = numArray.Split(' ');
+    int[] intArr = Array.ConvertAll(strArr, int.Parse);
+    int count = 0;
+    for (int i = 0; i < intArr.Length; i++)
+    {
+        if(intArr[i] > 0)  count++;
+    }
+    return count;
+}
+
+Console.Write("Type the numbers separated by a space and press Enter: ");
+string arrayString = Console.ReadLine()!;
+Console.Clear();
+int positiveNumCount = PositiveNumCount(arrayString);
+Console.Write($"{arrayString} -> {positiveNumCount}");
